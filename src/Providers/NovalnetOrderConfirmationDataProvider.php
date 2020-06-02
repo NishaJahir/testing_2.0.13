eff39262dcbf11942047001bad73758ddb0be556
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-06-02 12:05:47 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -100,13 +100,15 @@ class NovalnetOrderConfirmationDataProvider
                         }
                     }
                     $get_transaction_details = $transactionLog->getTransactionData('orderNo', $orderId);
+                    $totalCallbackAmount = 0;
                     foreach ($get_transaction_details as $transaction_details) {
-                        $payment_details = $transaction_details;
+                       $totalCallbackAmount += $transaction_details->callbackAmount;
                     }
-                    if (in_array($bank_details['paymentName'], ['novalnet_invoice', 'novalnet_prepayment']) && in_array($tid_status, ['91', '100']) && ($payment_details->amount > $payment_details->callbackAmount)) {
+                    
+                    if(in_array($tid_status, ['91', '100']) && ($db_details['payment_id'] == '27' && ($transaction_details->amount > $totalCallbackAmount) || $db_details['payment_id'] == '41') ) {
                         $comments .= PHP_EOL . $paymentService->getInvoicePrepaymentComments($bank_details);
                     }
-                    if($db_details['payment_id'] == '59' && ($payment_details->amount > $payment_details->callbackAmount) ) {
+                    if($db_details['payment_id'] == '59' && ($transaction_details->amount > $totalCallbackAmount)) {
                         $comments .= $cashpayment_comments;
                     }
                 }
